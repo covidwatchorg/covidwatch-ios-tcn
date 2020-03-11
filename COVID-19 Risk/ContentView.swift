29a0886b42e24ae5ca8e0437bf286e709e3c6bd0
Zsombor Szabo <zsombor@gmail.com>
2020-03-11 14:02:07 +0200

    Initial Commit

---
+++ COVID-19 Risk/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  Created by Zsombor Szabo on 11/03/2020.
+//  Copyright © 2020 IZE. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
