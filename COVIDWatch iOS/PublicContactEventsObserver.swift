d0ab5b8b6494bd91da80fc14c066ab2cf358f00f
Zsombor Szabo <zsombor@gmail.com>
2020-03-18 18:04:33 +0200

    Fix crash related to too many SQL variables in query Modify public contact event query to request only objects from the past 2 weeks

--- COVIDWatch iOS/PublicContactEventsObserver.swift
+++ COVIDWatch iOS/PublicContactEventsObserver.swift
@@ -12,7 +12,10 @@ open class PublicContactEventsObserver: NSObject {
     
     override init() {
         super.init()
-        Firestore.firestore().collection(Firestore.Collections.contactEvents).addSnapshotListener { [weak self] (querySnapshot, error) in
+        // Only observe the contact events from the past 2 weeks
+        Firestore.firestore().collection(Firestore.Collections.contactEvents)
+            .whereField(Firestore.Fields.timestamp, isGreaterThan: Timestamp(date: Date().addingTimeInterval(-60*60*24*7*2)))
+            .addSnapshotListener { [weak self] (querySnapshot, error) in
             guard let self = self else { return }
             if let error = error {
                 os_log("Listening for realtime updates of contact events failed: %@", type: .error, error as CVarArg)
@@ -20,32 +23,40 @@ open class PublicContactEventsObserver: NSObject {
             }
             guard let querySnapshot = querySnapshot else { return }
             os_log("Listened for realtime updates of %d contact event(s)", type: .info, querySnapshot.count)
-            let addedDocuments = querySnapshot.documentChanges.filter({ $0.type == .added }).map({ $0.document })
-            self.markLocalContactEvents(from: addedDocuments, asPotentiallyInfectious: true)
-            let removedDocuments = querySnapshot.documentChanges.filter({ $0.type == .removed }).map({ $0.document })
-            self.markLocalContactEvents(from: removedDocuments, asPotentiallyInfectious: false)
+            DispatchQueue.global(qos: .background).async {
+                let addedDocuments = querySnapshot.documentChanges.filter({ $0.type == .added }).map({ $0.document })
+                self.markLocalContactEvents(from: addedDocuments, asPotentiallyInfectious: true)
+                let removedDocuments = querySnapshot.documentChanges.filter({ $0.type == .removed }).map({ $0.document })
+                self.markLocalContactEvents(from: removedDocuments, asPotentiallyInfectious: false)
+            }
         }
     }
     
     private func markLocalContactEvents(from queryDocumentSnapshots: [QueryDocumentSnapshot], asPotentiallyInfectious infectious: Bool) {
         guard !queryDocumentSnapshots.isEmpty else { return }
-        let identifiers: [UUID] = queryDocumentSnapshots.compactMap({ UUID(uuidString: $0.documentID) })
         let context = PersistentContainer.shared.newBackgroundContext()
         context.perform {
             do {
-                let batchUpdateRequest = NSBatchUpdateRequest(entity: ContactEvent.entity())
-                batchUpdateRequest.predicate = NSPredicate(format: "identifier IN %@ AND wasPotentiallyInfectious != %d", identifiers, infectious)
-                batchUpdateRequest.resultType = .updatedObjectIDsResultType
-                batchUpdateRequest.propertiesToUpdate = [
-                    "wasPotentiallyInfectious" : infectious,
-                    // Marking a contact event as not infectious means that it was deleted from the public contact event list.
-                    // This means we need the update its uploadState locally.
-                    "uploadState" : infectious ? UploadState.uploaded.rawValue : UploadState.notUploaded.rawValue,
-                ]
-                let batchUpdateResult = try context.execute(batchUpdateRequest) as! NSBatchUpdateResult
-                let updatedObjectIDs = batchUpdateResult.result as! [NSManagedObjectID]
-                guard !updatedObjectIDs.isEmpty else { return }
-                NSManagedObjectContext.mergeChanges(fromRemoteContextSave: [NSUpdatedObjectsKey: updatedObjectIDs], into: [PersistentContainer.shared.viewContext])
+                let identifiers: [UUID] = queryDocumentSnapshots.compactMap({ UUID(uuidString: $0.documentID) })
+                os_log("Marking %d contact event(s) as potentially infectious=%d ...", type: .info, identifiers.count, infectious)
+                var allUpdatedObjectIDs = [NSManagedObjectID]()
+                try identifiers.chunked(into: 300000).forEach { (identifiers) in
+                    let batchUpdateRequest = NSBatchUpdateRequest(entity: ContactEvent.entity())
+                    batchUpdateRequest.predicate = NSPredicate(format: "identifier IN %@ AND wasPotentiallyInfectious != %d", identifiers, infectious)
+                    batchUpdateRequest.resultType = .updatedObjectIDsResultType
+                    batchUpdateRequest.propertiesToUpdate = [
+                        "wasPotentiallyInfectious" : infectious,
+                        // Marking a contact event as not infectious means that it was deleted from the public contact event list.
+                        // This means we need the update its uploadState locally.
+                        "uploadState" : infectious ? UploadState.uploaded.rawValue : UploadState.notUploaded.rawValue,
+                    ]
+                    let batchUpdateResult = try context.execute(batchUpdateRequest) as! NSBatchUpdateResult
+                    let updatedObjectIDs = batchUpdateResult.result as! [NSManagedObjectID]
+                    allUpdatedObjectIDs.append(contentsOf: updatedObjectIDs)
+                }
+                if !allUpdatedObjectIDs.isEmpty {
+                    NSManagedObjectContext.mergeChanges(fromRemoteContextSave: [NSUpdatedObjectsKey: allUpdatedObjectIDs], into: [PersistentContainer.shared.viewContext])
+                }
                 os_log("Marked %d contact event(s) as potentially infectious=%d", type: .info, queryDocumentSnapshots.count, infectious)
             }
             catch {
---
+++ COVIDWatch iOS/Utility/Array+Chunked.swift
@@ -0,0 +1,13 @@
+//
+//  Created by Zsombor Szabo on 18/03/2020.
+//
+
+import Foundation
+
+extension Array {
+    func chunked(into size: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: size).map {
+            Array(self[$0 ..< Swift.min($0 + size, count)])
+        }
+    }
+}
