36efc07ee91d50f9e661a088131d235776c916ed
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2020-04-08 14:09:37 -0700

    using UI components but now it doesn't scale properly to iphone 8. Going to go back through with my Figma proportion method and see if that fixes it

--- COVIDWatch iOS/ViewControllers/Shared.swift
+++ COVIDWatch iOS/ViewControllers/Shared.swift
@@ -11,29 +11,16 @@ import UIKit
 class share: BaseViewController {
     var logo: UIImageView?
     var img: UIImageView?
-    var share = UILabel()
-    var share2 = UITextView()
-    
-    var spread = UIView()
-    var spreadLbl = UILabel()
-    var spreadLbl2 = UILabel()
-    
-    var tested = UIView()
-    var testedLbl = UILabel()
-    var testedLbl2 = UITextView()
-    
+    var share = LargeText()
+    var share2 = MainText()
+    var spread = Button(text: "Spread the word", subtext: "It works best when everyone uses it.")
+    var tested = Button(text: "Tested for COVID-19?", subtext: "Share your result anonymously to help keep your community stay safe.")
     let screenSize: CGRect = UIScreen.main.bounds
     var scalingFactor: CGFloat?
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
         self.scalingFactor = CGFloat(screenHeight) / CGFloat(896)
-        var fontScalingFactor = CGFloat(1.0)
-        if screenHeight <= 736.0 { fontScalingFactor = scalingFactor! }
-        print("ScreenHeight = \(screenHeight)")
-        
-        var imageb: UIImage = UIImage(named: "woman-hero-blue-2")!
+        let imageb: UIImage = UIImage(named: "woman-hero-blue-2")!
         img = UIImageView(image: imageb)
         img!.frame.size.width = 253
         img!.frame.size.height = 259
@@ -45,81 +32,31 @@ class share: BaseViewController {
         
         
         share.text =  "Share & Protect"
-        share.textColor = UIColor(red: 0.345, green: 0.345, blue: 0.345, alpha: 1)
-        share.font = UIFont(name: "Montserrat-SemiBold", size: 36 * fontScalingFactor)
-        share.sizeToFit()
+        share.frame.size.height = share.contentSize.height
         share.center.x = view.center.x
         share.center.y = img!.center.y + img!.frame.size.height/2 + 40
         
         view.addSubview(share)
         
         share2.text =  "Covid Watch is using bluetooth to anonymously watch who you come in contact with. You will be notified of potential contact to COVID-19."
-        share2.textColor = UIColor(red: 0.345, green: 0.345, blue: 0.345, alpha: 1)
-        share2.font = UIFont(name: "Montserrat-Regular", size: 18 * fontScalingFactor)
-        share2.frame.size.width = 300
-        share2.frame.size.height = 125
+        share2.frame.size.height = share2.contentSize.height
         share2.center.x = view.center.x
         share2.center.y = share.center.y + share.frame.size.height/2 + 60
-        share2.textAlignment = .center
         share2.isEditable = false
         view.addSubview(share2)
         
-        spread.frame.size.width = share2.frame.size.width
-        spread.frame.size.height = 58
         spread.center.x = view.center.x
         spread.center.y = share2.center.y + share2.frame.size.height/2 + 40
-        spread.backgroundColor = UIColor(red: 0.286, green: 0.435, blue: 0.714, alpha: 1)
-        spread.layer.cornerRadius = 10
-        //    self.spread.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.nextScreen)))
-        view.addSubview(spread)
-        spreadLbl.text =  "Spread the word"
-        spreadLbl.textColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
-        spreadLbl.font = UIFont(name: "Montserrat-Bold", size: 18 * fontScalingFactor)
-        spreadLbl.sizeToFit()
-        spreadLbl.center.x = spread.center.x
-        spreadLbl.center.y = spread.center.y
+        spread.draw(parentVC: self)
         
-        view.addSubview(spreadLbl)
-        
-        spreadLbl2.text =  "It works best when everyone uses it."
-        spreadLbl2.textColor = UIColor(red: 0.345, green: 0.345, blue: 0.345, alpha: 1)
-        spreadLbl2.font = UIFont(name: "Montserrat-Regular", size: 14 * fontScalingFactor)
-        spreadLbl2.sizeToFit()
-        spreadLbl2.center.x = spread.center.x
-        spreadLbl2.center.y = spreadLbl.center.y + spreadLbl.frame.size.height/2 + 40
-        
-        view.addSubview(spreadLbl2)
-        
-        tested.frame.size.width = share2.frame.size.width
-        tested.frame.size.height = 58
         tested.center.x = view.center.x
-        tested.center.y = spreadLbl2.center.y + spreadLbl2.frame.size.height/2 + 40
+        tested.center.y = spread.subtext!.center.y + spread.subtext!.frame.size.height/2 + 40
         tested.backgroundColor = .clear
         tested.layer.borderWidth = 1
         tested.layer.borderColor = UIColor(red: 0.8, green: 0.8, blue: 0.8, alpha: 1).cgColor
-        tested.layer.cornerRadius = 10
-         self.tested.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.test)))
-        view.addSubview(tested)
-        
-        testedLbl.text =  "Tested for COVID-19?"
-        testedLbl.textColor = UIColor(red: 0.345, green: 0.345, blue: 0.345, alpha: 1)
-        testedLbl.font = UIFont(name: "Montserrat-Bold", size: 18 * fontScalingFactor)
-        testedLbl.sizeToFit()
-        testedLbl.center.x = tested.center.x
-        testedLbl.center.y = tested.center.y
-        
-        view.addSubview(testedLbl)
-        
-        testedLbl2.text =  "Share your result anonymously to help your community stay safe."
-        testedLbl2.textColor = UIColor(red: 0.345, green: 0.345, blue: 0.345, alpha: 1)
-        testedLbl2.font = UIFont(name: "Montserrat-Regular", size: 14 * fontScalingFactor)
-        testedLbl2.frame.size.width = tested.frame.size.width
-        testedLbl2.frame.size.height = 100
-        testedLbl2.center.x = spread.center.x
-        testedLbl2.center.y = testedLbl.center.y + testedLbl.frame.size.height/2 + 80
-        testedLbl2.textAlignment = .center
-         testedLbl2.isEditable = false
-        view.addSubview(testedLbl2)
+        tested.text.textColor = UIColor(red: 0.345, green: 0.345, blue: 0.345, alpha: 1)
+        self.tested.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.test)))
+        tested.draw(parentVC: self)
     }
     @objc func test(){
          performSegue(withIdentifier: "test", sender: self)
