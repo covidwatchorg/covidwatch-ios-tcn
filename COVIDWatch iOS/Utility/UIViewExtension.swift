d7e312475a9dc916e7660c120689b85823b9e618
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2020-04-09 13:13:09 -0700

    consolidated VC and View extensions into Responder extension, added some new extension features

--- COVIDWatch iOS/Utility/UIViewExtension.swift
+++ COVIDWatch iOS/Utility/UIViewExtension.swift
@@ -14,37 +14,20 @@ enum LinePosition {
 }
 
 extension UIView {
-//    let screenWidth = screenWidth
-//    let screenHeight = screenHeight
-    var screenWidth: CGFloat {
-        get {
-            return UIScreen.main.bounds.width
-        }
-    }
-    var screenHeight: CGFloat {
-        get {
-            return UIScreen.main.bounds.height
-        }
-    }
-    var contentMaxWidth: CGFloat {
-        get {
-            screenWidth * (321.0/375.0)
-        }
-    }
-
+    
     func addLine(position: LinePosition, color: UIColor, width: Double) {
         let lineView = UIView()
         lineView.backgroundColor = color
         lineView.translatesAutoresizingMaskIntoConstraints = false // This is important!
         self.addSubview(lineView)
-
+        
         let metrics = ["width": NSNumber(value: width)]
         let views = ["lineView": lineView]
         self.addConstraints(NSLayoutConstraint.constraints(
             withVisualFormat: "H:|[lineView]|",
             options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: metrics, views: views)
         )
-
+        
         switch position {
         case .top:
             self.addConstraints(
