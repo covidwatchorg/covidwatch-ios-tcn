738abe4b95145a149f790534ce1eb29b1dfbf551
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2020-04-10 11:07:00 -0700

    added user state and beginning of the logic for managing it

--- COVIDWatch iOS/ViewControllers/Share.swift
+++ COVIDWatch iOS/ViewControllers/Share.swift
@@ -7,6 +7,15 @@
 //
 
 import UIKit
+enum TestedState {
+    case notTested, testedPositive, testedNegative
+}
+
+struct UserState {
+    var firstTimeUser: Bool
+    var testedState: TestedState
+    var hasBeenInContact: Bool
+}
 
 class Share: BaseViewController {
     var img = UIImageView(image: UIImage(named: "woman-hero-blue-2"))
@@ -16,10 +25,20 @@ class Share: BaseViewController {
     var spreadButton = Button(text: "Spread the word", subtext: "It works best when everyone uses it.")
     var testedButton = Button(text: "Tested for COVID-19?",
                               subtext: "Share your result anonymously to help keep your community stay safe.")
-    let screenSize: CGRect = UIScreen.main.bounds
+    var infoBanner = InfoBanner(text: "You may have been in contact with COVID-19")
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
 
+//        TODO: for testing purposes only; user state should be stored and managed globally
+        var userState = UserState(firstTimeUser: true, testedState: .notTested, hasBeenInContact: false)
+        
+        drawScreen(userState: userState)
+    }
+    @objc func test() {
+         performSegue(withIdentifier: "test", sender: self)
+    }
+    
+    private func drawScreen(userState: UserState) {
         img = UIImageView(image: UIImage(named: "woman-hero-blue-2"))
         img.frame.size.width = 253 * figmaToiOSHorizontalScalingFactor
         img.frame.size.height = 259 * figmaToiOSVerticalScalingFactor
@@ -43,17 +62,15 @@ class Share: BaseViewController {
                           centerX: view.center.x,
                           originY: mainText.frame.maxY + (5.0 * figmaToiOSVerticalScalingFactor))
 
-        self.testedButton.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.test)))
-        let testedButtonTop: CGFloat = 668.0 * figmaToiOSVerticalScalingFactor
-        testedButton.draw(parentVC: self, centerX: view.center.x, originY: testedButtonTop)
-
-        testedButton.backgroundColor = .clear
-        testedButton.layer.borderWidth = 1
-        testedButton.layer.borderColor = UIColor(red: 0.8, green: 0.8, blue: 0.8, alpha: 1).cgColor
-        testedButton.text.textColor = UIColor(red: 0.345, green: 0.345, blue: 0.345, alpha: 1)
-    }
-    @objc func test() {
-         performSegue(withIdentifier: "test", sender: self)
+        if userState.testedState == .notTested {
+            self.testedButton.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.test)))
+            let testedButtonTop: CGFloat = 668.0 * figmaToiOSVerticalScalingFactor
+            testedButton.draw(parentVC: self, centerX: view.center.x, originY: testedButtonTop)
+            testedButton.backgroundColor = .clear
+            testedButton.layer.borderWidth = 1
+            testedButton.layer.borderColor = UIColor(red: 0.8, green: 0.8, blue: 0.8, alpha: 1).cgColor
+            testedButton.text.textColor = UIColor(red: 0.345, green: 0.345, blue: 0.345, alpha: 1)
+        }
     }
 
 }
