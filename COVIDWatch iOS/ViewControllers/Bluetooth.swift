bdab604b55d5f7570b3b645d169722d6b8f81596
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2020-04-18 22:15:58 -0700

    deleting comment

--- COVIDWatch iOS/ViewControllers/Bluetooth.swift
+++ COVIDWatch iOS/ViewControllers/Bluetooth.swift
@@ -91,15 +91,4 @@ class Bluetooth: BaseViewController {
             }
         }
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
